a42b611e2020f1b01b372ee46a2cca5c1562c3d5
holidaynerd74 <m4tr9@Khalids-MacBook-Air.local>
2014-05-23 15:48:00 -0400

    Khalid

--- public/js/mediaPlayer/pureHtml5Player.html.js
+++
@@ -1,9 +0,0 @@
-angular.module("directives/mediaPlayer/pureHtml5Player.tpl.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("directives/mediaPlayer/pureHtml5Player.tpl.html",
-    "<div class=\"pureHTML5Player\">\n" +
-    "   <video autoplay>\n" +
-    "     <source type=\"video/mp4\" ng-src=\"{{trustSrc(videoConfig.playlist[0], '.mp4')}}\">\n" +
-    "     <source type=\"video/ogg\" ng-src=\"{{trustSrc(videoConfig.playlist[0], '.ogv')}}\">\n" +
-    "   </video>\n" +
-    "</div>");
-}]);
